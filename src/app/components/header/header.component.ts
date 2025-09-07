5981f984dfe0782ab8f4f81bf9b92c7fd829b7f2
Victor Wanderley <victor.wanderley1709@gmail.com>
2025-09-07 01:33:16 -0300

    fix: Otimizacao para o budget

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,13 +6,17 @@ import { Component, HostListener } from '@angular/core';
   standalone: true,
   imports: [NgOptimizedImage, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   isScrolled: boolean = false;
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    const scrollOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const scrollOffset =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
     this.isScrolled = scrollOffset > 100; // Aumenta o threshold para 100px
   }
 }
